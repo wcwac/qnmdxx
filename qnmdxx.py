99be277116a52d7904d87a4208228f89d53e31f2
wcwac <16619878+wcwac@users.noreply.github.com>
2020-11-10 14:01:58 +0800

    Update qnmdxx.py

--- qnmdxx.py
+++ qnmdxx.py
@@ -16,7 +16,6 @@ option.add_argument("--no-sandbox")
 option.add_argument("--headless")
 driver = webdriver.Chrome(chrome_options=option)
 driver.set_window_size(640 , 960)
-print(url + '/icon/icon.png')
 driver.get(url + '/icon/icon.png')
 driver.delete_all_cookies()
 
